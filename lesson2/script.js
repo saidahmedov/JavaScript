341a6802c365bc2bfdff1795c2823d74d60dd115
Said Ahmed <89637007721@mail.ru>
2019-08-29 20:24:39 +0300

    загразил предыдущие уроки на Git

---
+++ lesson2/script.js
@@ -0,0 +1,31 @@
+ 'use strict';
+
+
+let money = 70000;
+console.log(money);
+console.log(typeof money);
+
+let income = '150000';
+console.log(typeof income);
+console.log(income.length);
+
+let period = 7;
+console.log(period);
+
+let mission = 350000;
+console.log(mission);
+
+let addExpenses = 'КОМПЬЮТЕР, МАШИНА, КВАРТИРА';
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(' '));
+
+let deposit = true;
+
+let budgetDay = 70000;
+console.log(budgetDay / 30);
+console.log(budgetDay % 30);
+
+
+
+
+
