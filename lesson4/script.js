434397f5f2d5791720ede3df614c4f3617f7232f
Said Ahmed <89637007721@mail.ru>
2019-08-29 20:30:51 +0300

    удалил лишних консолей и исправил код

--- lesson4/script.js
+++ lesson4/script.js
@@ -1,38 +1,35 @@
-let mission = 350000;
-console.log(mission);
-let income = '150000';
-
-let money = +prompt('Ваш ежемесячный доход?', 100000);
-console.log(money);
-
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.', 'Еда, еда и еще раз еда))');
-console.log([addExpenses]);
-
-let deposit = confirm('Есть ли у вас депозит в банке?');
-console.log(deposit);
-
-let showTypeOf = function (data) {
-	console.log(data, typeof(data));
+let mission = 350000,
+	 income = 'Freelance',
+	 money = +prompt('Ваш ежемесячный доход?', 100000),
+	 addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую.', 'Еда, еда и еще раз еда))'),
+	 deposit = confirm('Есть ли у вас депозит в банке?');
+	
+let showTypeOf = function (data){
+		console.log(data, typeof(data));
 }
-showTypeOf(money);
-showTypeOf(deposit);
-showTypeOf(income);
+	showTypeOf(money);
+	showTypeOf(deposit);
+	showTypeOf(income);
 
 let costs = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-console.log(costs);
-let howMuchCost = +prompt('Во сколько это обойдется?', 3500);
-console.log(howMuchCost);
-let costs2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
-console.log(costs2);
-let howMuchCost2 = +prompt('Во сколько это обойдется?', 2000);
-console.log(howMuchCost2);
-let budgetMonth = money - howMuchCost - howMuchCost2;
-console.log(budgetMonth);
-console.log(Math.ceil(mission / budgetMonth ));
-console.log(Math.floor(budgetMonth / 30));
+	 howMuchCost = +prompt('Во сколько это обойдется?', 3500);
+	 costs2 = prompt('Какие обязательные ежемесячные расходы у вас есть?');
+	 howMuchCost2 = +prompt('Во сколько это обойдется?', 2000);
+	 budgetMonth = money - howMuchCost - howMuchCost2;
+ 	 budgetDay = Math.floor(budgetMonth / 30);
 
-let budgetDay = Math.floor(budgetMonth / 30);
-
+	let getExpensesMonth = function() {
+		return howMuchCost + howMuchCost2;
+	};
+		console.log(getExpensesMonth());
+	let getAccumulatedMonth = function (){
+		return money - getExpensesMonth();
+	};
+		console.log(getAccumulatedMonth());
+	let getTargetMonth = function(){
+		return mission / getAccumulatedMonth();
+	};
+		console.log(Math.floor(getTargetMonth()));
 let getStatusIncome = function (){
 	if (budgetDay >= 800){
 		return('Высокий уровень дохода');
@@ -45,17 +42,3 @@ let getStatusIncome = function (){
 	}
    };
    	console.log(getStatusIncome());
-
-
-	let getExpensesMonth = function() {
-		return howMuchCost + howMuchCost2;
-	};
-		console.log(getExpensesMonth());
-	let getAccumulatedMonth = function () {
-		return money - getExpensesMonth();
-	};
-		console.log(getAccumulatedMonth());
-	let getTargetMonth = function(){
-		return mission / getAccumulatedMonth();
-	};
-		console.log(Math.floor(getTargetMonth()));
