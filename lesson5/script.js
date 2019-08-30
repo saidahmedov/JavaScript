ed9928a9dcaa9f1e21ceb3b178e9aa344e8349cc
Said Ahmed <89637007721@mail.ru>
2019-08-30 17:48:57 +0300

    пятый урок

---
+++ lesson5/script.js
@@ -0,0 +1,63 @@
+'use striect';
+
+let money,
+	mission = 350000,
+	income = 'Freelance',
+	addExpenses = prompt('Перечислите возможные расходы через запятую.', 'Колледж, Кварплата'),
+	deposit = confirm('Есть ли у вас депозит в банке?');
+	let start = function() {
+		do {
+			money = prompt('Ваш ежемесячный доход?', 100000);
+		}
+		while(isNaN(money) || money === '' || money === null){};
+	}
+	start();
+let showTypeOf = function (item){
+		console.log(item, typeof(item));
+};
+	showTypeOf(money);
+	showTypeOf(deposit);
+	showTypeOf(income);
+let costs,
+	costs2;
+	let getExpensesMonth = function() {
+		let sum = 0;
+			for (let i = 0; i < 2; i++) {
+				if (i === 0) {
+					costs = prompt('Какие обязательные ежемесячные расходы у вас есть?', "Колледж");
+				}
+				else if (i === 1) {
+					costs2 = prompt('Какие обязательные ежемесячные расходы у вас есть?', "Кварплата");
+				}
+				while (isNaN(getExpensesMonth) || getExpensesMonth === '' || getExpensesMonth === null){
+					sum += +prompt('Во сколько это обойдется?', 13500);
+				};
+
+			};
+				
+		return sum;
+	};
+	let expensesAmount = getExpensesMonth();
+		console.log('Все расходы за месяц: ', expensesAmount);
+	let getAccumulatedMonth = function (){
+		return money - expensesAmount;
+	};
+		console.log('Ежемесячный доход со всеми расходами: ', getAccumulatedMonth());
+	let getTargetMonth = function(){
+		return mission / getAccumulatedMonth();
+	};
+		console.log('За столько месяцев будет достигнута цель: ', Math.floor(getTargetMonth()));
+	let budgetDay = getAccumulatedMonth() / 30;
+		console.log('Ежедневный доход: ', Math.floor(budgetDay));
+let getStatusIncome = function (){
+	if (budgetDay >= 800){
+		return('Высокий уровень дохода');
+	}else if ((budgetDay >= 300) && (budgetDay < 800)){
+	 return('Средний уровень дохода');
+	}else if ((budgetDay >= 0) && (budgetDay < 300)){
+	 return('Низкий уровень дохода');
+	}else {
+	 return('Что то пошло не так');
+	}
+   };
+   	console.log(getStatusIncome());
